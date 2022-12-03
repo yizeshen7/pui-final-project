31f037333e208c5080e64850416d5044723b8e9a
Yize Shen <yizeshen7@gmail.com>
2022-12-03 16:28:51 -0500

    added nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    textColor: {
+      primary: "#3C4443",
+      secondary: "#F5BE7A",
+      tertiary: "#96BBE4",
+      neutral1: "#1D211C",
+      neutral2: "#454F41",
+      neutral3: "#454F41",
+      neutral4: "#BFC6BE",
+      neutral5: "#EFF1EF",
+    },
+  },
+};
