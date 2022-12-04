b703aae19d83e6594d8349564f7cc59c73b4d191
Yize Shen <yizeshen7@gmail.com>
2022-12-03 19:20:17 -0500

    completed first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/index.html"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
   theme: {
+    colors: {
+      primary: "#3C4443",
+      secondary: "#F5BE7A",
+      tertiary: "#96BBE4",
+      neutral1: "#1D211C",
+      neutral2: "#454F41",
+      neutral3: "#454F41",
+      neutral4: "#BFC6BE",
+      neutral5: "#EFF1EF",
+    },
     textColor: {
       primary: "#3C4443",
       secondary: "#F5BE7A",
